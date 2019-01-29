9a1fd265149ba571975de50c709e3a325c9772c7
a <eahilendran@gmail.com>
2019-01-29 10:22:10 +0530

    Firebase setup and folder setup

---
+++ SwiftBikersFirbaseAppStarter/Controllers/NewsFeedVC.swift
@@ -0,0 +1,30 @@
+//
+//  NewsFeedVC.swift
+//  SwiftBikersFirbaseAppStarter
+//
+//  Created by MacBook on 1/29/19.
+//  Copyright © 2019 Ahil. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
